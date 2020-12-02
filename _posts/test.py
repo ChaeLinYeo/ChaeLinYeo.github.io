8efc0ffaf7d3a12e0475718be493e1efc9d47a62
ChaeLinYeo <evergreen786@naver.com>
2020-12-03 00:13:01 +0900

    upload

--- _posts/test.py
+++ _posts/test.py
@@ -1,20 +1,24 @@
 def solution(numbers):
     answer = ''
-    d = {} # 원래의 숫자가 키, 변형된 숫자가 값으로 들어갈 사전
-    origin = list(map(str, numbers)) # 원래의 수를 담아둘 오리진 배열
-    numbers = list(map(str, numbers)) # numbers의 숫자를 문자열로 바꿈
-    for i in range(len(numbers)):
-        numbers[i]=numbers[i]*3
-        numbers[i]=numbers[i][:4]
-        d[origin[i]] = numbers[i]
-    print(numbers)
-
-    d = sorted(d.items(), reverse=True, key=lambda item:item[1]) # 딕셔너리의 값을 기준으로 내림차순 정렬
-    print(d)
-    for i in range(len(d)):
-        answer += d[i][0]
-
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x : (x*4)[:4], reverse = True)
+    if numbers[0] == '0':
+        answer = '0'
+    else:
+        answer = ''.join(numbers)
     return answer
 
-# print(solution([6, 00, 2]))
-print(solution([3, 30, 34, 5, 9])) # 9534330
+# def solution(numbers):
+#     numbers = [str(x) for x in numbers] # 문자열로 변환. O(n)복잡도를 가진다.
+#     numbers.sort(key=lambda x : (x*4)[:4], reverse = True) # x라는 원소가 주어지면 4번 반복하고 앞에서 4개까지를 슬라이스해서 딱 4개에 맞게 떨어지도록 만든다. 그리고 내림차순으로 정렬한다. O(nlogn)복잡도를 가진다.
+#     print(numbers)
+#     if numbers[0] == '0':
+#         answer = '0'
+#         # 만약 0만 두개 이상 들어있는 배열이 주어진다면, 00, 0000 이런게 나오게 될텐데, 그럴때는 0 한글자로 이루어진 문자열을 리턴하게 해야한다.
+#     else:
+#         answer = ''.join(numbers)
+#     # if에 의한 수행은 O(n)
+#     return answer
+
+print(solution([6, 00, 2]))
+# print(solution([3, 30, 34, 5, 9])) # 9534330
