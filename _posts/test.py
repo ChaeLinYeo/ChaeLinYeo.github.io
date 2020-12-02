508bff48db06f7c41f8ef7f3c6ae1ab4c12b180f
ChaeLinYeo <evergreen786@naver.com>
2020-12-02 22:15:57 +0900

    upload

--- _posts/test.py
+++ _posts/test.py
@@ -1,22 +1,20 @@
-def solution(n, lost, reserve):
-    answer = 0
-    u = [1]*(n+2) # 학생의 유니폼 소지 여부 모두 1으로 초기화
-    for r in reserve: # 여벌 갖고 있는사람 +1
-        u[r]+=1
-    for l in lost: # 도난당한 사람 -1
-        u[l]-=1
-    for i in range(len(u)): #  여분이 있는 사람은 자기보다 번호가 작은 사람에게 한벌을 빌려줌
-        if u[i]==0 and u[i+1]==2:
-            u[i]+=1
-            u[i+1]-=1
-    for i in range(len(u)):#  여분이 있는 사람은 자기보다 번호가 큰 사람에게 한벌을 빌려줌
-        if u[i]==2 and u[i+1]==0:
-            u[i]-=1
-            u[i+1]+=1
-    # print(u)
-    for j in range(len(u)):
-        if u[j]!=0:
-            answer +=1
-    return answer-2
+def solution(numbers):
+    answer = ''
+    d = {} # 원래의 숫자가 키, 변형된 숫자가 값으로 들어갈 사전
+    origin = list(map(str, numbers)) # 원래의 수를 담아둘 오리진 배열
+    numbers = list(map(str, numbers)) # numbers의 숫자를 문자열로 바꿈
+    for i in range(len(numbers)):
+        numbers[i]=numbers[i]*3
+        numbers[i]=numbers[i][:4]
+        d[origin[i]] = numbers[i]
+    print(numbers)
 
-print(solution(5, [2,4], [1,3,5]))#5
+    d = sorted(d.items(), reverse=True, key=lambda item:item[1]) # 딕셔너리의 값을 기준으로 내림차순 정렬
+    print(d)
+    for i in range(len(d)):
+        answer += d[i][0]
+
+    return answer
+
+# print(solution([6, 00, 2]))
+print(solution([3, 30, 34, 5, 9])) # 9534330
